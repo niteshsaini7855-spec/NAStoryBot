d67d0fc82e79886eb61eb71f862b859fae6f2837
niteshsaini7855-spec <niteshsaini7855@gmail.com>
2026-07-14 12:26:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 
-# Telegram Bot Configurations
-BOT_TOKEN = "8790789479:AAHmA79wrgUFwVBCtJTWvxLuyL1aH45u-To"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 INSTAGRAM_LINK = "https://instagram.com/niteshsaini009"
 YOUTUBE_LINK = "https://www.youtube.com/channel/UCM7ds25KyXVLbtKjrsOrh7A"
 
-# Admin Usernames/IDs
 ADMIN1_ID = "@NSDEVIL"
 ADMIN2_ID = "@arundixit2005"
